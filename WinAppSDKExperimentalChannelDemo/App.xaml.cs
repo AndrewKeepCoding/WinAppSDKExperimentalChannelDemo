3c02b0556e25741506e0eff49cef5be664d138bc
Andrew KeepCoding <andrewkeepcoding@gmail.com>
2024-07-12 23:03:05 +0900

    Add project files.

---
+++ WinAppSDKExperimentalChannelDemo/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.UI.Xaml;
+
+namespace WinAppSDKExperimentalChannelDemo;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        Microsoft.Windows.Globalization.ApplicationLanguages.PrimaryLanguageOverride = "ja";
+    }
+
+    protected override void OnLaunched(LaunchActivatedEventArgs args)
+    {
+        _window = new MainWindow();
+        _window.Activate();
+    }
+
+    private Window _window;
+}
---
+++ WinAppSDKExperimentalChannelDemo/MainWindow.xaml.cs
@@ -0,0 +1,11 @@
+using Microsoft.UI.Xaml;
+
+namespace WinAppSDKExperimentalChannelDemo;
+
+public sealed partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
